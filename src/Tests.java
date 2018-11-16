c88217ea49bd6b31d20b95ba4eaf8f1e074c9aef
Guillaume VANNESSON <guillaume.vannesson@telecomnancy.eu>
2018-11-16 09:46:03 +0100

    Modification des commentaires (qui ne fonctionnent toujours pas) et ajout de tests sur la règle

--- src/Tests.java
+++ src/Tests.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -10,13 +11,16 @@ import org.junit.Test;
 
 public class Tests {
 
+	private String rep="Tests/";
 	/*@Before
 	public void setUp() throws Exception {
 	}*/
 
-	@Test
+	/*@Test
 	public void testCommentaires() throws IOException{
-		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("Tests/testCommentaires/test1.tig"));
+		System.out.println("testCommentaires");
+		String rep="testCommentaires/";
+		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test1.tig"));
         TigerLexer lexer = new TigerLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         TigerParser parser = new TigerParser(tokens);
@@ -25,6 +29,46 @@ public class Tests {
 		} catch (RecognitionException e) {
 			e.printStackTrace();
 		}
+	}*/
+	@Test
+	public void testCorrectsExp() throws IOException{
+		String rep="testExp/corrects/";
+		System.out.println("Nombre de fichiers de test:"+numberOfFiles(this.rep+rep));
+		for(int i=1;i<=numberOfFiles(this.rep+rep);i++)
+		{
+			System.out.println(this.rep+rep+"test"+i+".tig");
+			ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test"+i+".tig"));
+	        TigerLexer lexer = new TigerLexer(input);
+	        CommonTokenStream tokens = new CommonTokenStream(lexer);
+	        TigerParser parser = new TigerParser(tokens);
+	        try {
+				parser.program();
+			} catch (RecognitionException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	/*@Test
+	public void testIncorrectsExp() throws IOException{
+		System.out.println("testExp/incorrects");
+		String rep="testExp/incorrects";
+		for(int i=1;i<=numberOfFiles(rep);i++)
+		{
+			ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test"+i+".tig"));
+	        TigerLexer lexer = new TigerLexer(input);
+	        CommonTokenStream tokens = new CommonTokenStream(lexer);
+	        TigerParser parser = new TigerParser(tokens);
+	        try {
+				parser.program();
+			} catch (RecognitionException e) {
+				e.printStackTrace();
+			}
+		}
+	}*/
+	
+	public int numberOfFiles(String path)
+	{
+		return new File(path).listFiles().length;
 	}
 
 }
