ab5ebd2454f5c23d589e6a4a905ce950ef86e98e
Guillaume Vannesson <guillaume.vannesson3@etu.univ-lorraine.fr>
2018-11-17 21:44:53 +0100

    Modification des tests afin de modifier le flux de sortie pour récupérer les erreurs dans un buffer et réaliser des tests sur la longueur

--- src/Tests.java
+++ src/Tests.java
@@ -1,8 +1,10 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.antlr.runtime.*;
 
@@ -17,26 +19,43 @@ public class Tests {
 	}*/
 
 	@Test
-	public void testCommentaires() throws IOException{
-		System.out.println("testCommentaires");
-		String rep="testCommentaires/";
-		ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test1.tig"));
-        TigerLexer lexer = new TigerLexer(input);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        TigerParser parser = new TigerParser(tokens);
-        try {
-			parser.program();
-		} catch (RecognitionException e) {
-			e.printStackTrace();
-		}
-	}
-	@Test
-	public void testCorrectsExp() throws IOException{
-		String rep="testExp/corrects/";
+	public void testsFournis() throws IOException
+	{
+		String rep="testProf/";
 		System.out.println("Nombre de fichiers de test:"+numberOfFiles(this.rep+rep));
 		for(int i=1;i<=numberOfFiles(this.rep+rep);i++)
 		{
 			System.out.println(this.rep+rep+"test"+i+".tig");
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    PrintStream ps = new PrintStream(baos);
+		    PrintStream err = System.err;
+		    System.setErr(ps);
+			ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"prog"+i+".txt"));
+	        TigerLexer lexer = new TigerLexer(input);
+	        CommonTokenStream tokens = new CommonTokenStream(lexer);
+	        TigerParser parser = new TigerParser(tokens);
+	        try {
+				parser.program();
+			} catch (RecognitionException e) {
+				e.printStackTrace();
+			}
+	      //on restaure out
+	        System.out.flush();
+		    System.setErr(err);
+		    assertEquals(0, baos.toString().length());
+		}
+	}
+	@Test
+	public void testCommentaires() throws IOException{
+		System.out.println("testCommentaires");
+		String rep="testCommentaires/";
+		for(int i=1;i<=numberOfFiles(this.rep+rep);i++)
+		{
+			System.out.println(this.rep+rep+"test"+i+".tig");
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    PrintStream ps = new PrintStream(baos);
+		    PrintStream err = System.err;
+		    System.setErr(ps);
 			ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test"+i+".tig"));
 	        TigerLexer lexer = new TigerLexer(input);
 	        CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -46,7 +65,39 @@ public class Tests {
 			} catch (RecognitionException e) {
 				e.printStackTrace();
 			}
+	      //on restaure out
+	        System.out.flush();
+		    System.setErr(err);
+		    assertEquals(0, baos.toString().length());
 		}
+	}
+	@Test
+	public void testCorrectsExp() throws IOException{
+		String rep="testExp/corrects/";
+		System.out.println("Nombre de fichiers de test:"+numberOfFiles(this.rep+rep));
+		for(int i=1;i<=numberOfFiles(this.rep+rep);i++)
+		{
+			System.out.println(this.rep+rep+"test"+i+".tig");
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    PrintStream ps = new PrintStream(baos);
+		    PrintStream err = System.err;
+		    System.setErr(ps);
+			ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(this.rep+rep+"test"+i+".tig"));
+	        TigerLexer lexer = new TigerLexer(input);
+	        CommonTokenStream tokens = new CommonTokenStream(lexer);
+	        TigerParser parser = new TigerParser(tokens);
+	        try {
+				parser.program();
+			} catch (RecognitionException e) {
+				e.printStackTrace();
+			}
+	      //on restaure out
+	        System.out.flush();
+		    System.setErr(err);
+		    assertEquals(0, baos.toString().length());
+		}
+		
+		
 	}
 	/*@Test
 	public void testIncorrectsExp() throws IOException{
@@ -70,5 +121,6 @@ public class Tests {
 	{
 		return new File(path).listFiles().length;
 	}
+	
 
 }
