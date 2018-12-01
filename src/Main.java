a80daf9dbb8c0ab82fd5833cf23647e51c763341
Guillaume Vannesson <guillaume.vannesson@telecomnancy.eu>
2018-12-01 17:05:02 +0100

    Ajout de la classe Main pour contenir la méthode main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-public static void main(String[] args) {
-    TableSymboles blocOrig = new TableSymboles(null);
 
+public class Main {
+	public static void main(String[] args) {
+	    TableSymboles blocOrig = new TableSymboles(null);
+
+	}
 }
