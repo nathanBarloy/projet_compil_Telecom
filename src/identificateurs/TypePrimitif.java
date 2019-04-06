0c8bbbe867f94a9a5423bf8e62990c4fcf55ea3e
Guillaume Vannesson <guillaume.vannesson@telecomnancy.eu>
2019-04-06 17:02:55 +0200

    Correction d'une erreur de taille de type

--- src/identificateurs/TypePrimitif.java
+++ src/identificateurs/TypePrimitif.java
@@ -2,7 +2,7 @@ package identificateurs;
 
 public class TypePrimitif extends Type {
 	
-	public final static TypePrimitif INT = new TypePrimitif("int",4);// un entier est stocké sur 32 bits
+	public final static TypePrimitif INT = new TypePrimitif("int",2);// un entier est stocké sur 32 bits
 	public final static TypePrimitif STRING = new TypePrimitif("string", 2);//on stocke juste l'adresse du premier caractère
 	public final static TypePrimitif VOID = new TypePrimitif("void",0);//
 	
